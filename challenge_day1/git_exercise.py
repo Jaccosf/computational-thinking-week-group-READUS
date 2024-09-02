c6d63c7bfcecbd2091623e3a2ed386c6211380b7
Jaccosf <69802108+Jaccosf@users.noreply.github.com>
2024-09-02 12:52:29 +0200

    Add comma's

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -3,6 +3,6 @@ from merijn import merijn
 from dodo import function
 
 def teamName():
-    return "This is team READUS. We are: " + myName() + merijn() + function()
+    return "This is team READUS. We are: " + myName() + ", " + merijn() + ", " + function()
 
 print(teamName())
