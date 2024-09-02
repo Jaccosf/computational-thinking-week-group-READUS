78788168ffbbd5546f318100a81ef5df3e0ebb1b
Jaccosf <69802108+Jaccosf@users.noreply.github.com>
2024-09-02 12:30:42 +0200

    Create git_exercise.py
    Create a team name function

---
+++ challenge_day1/git_exercise.py
@@ -0,0 +1,8 @@
+from jacco import myName
+from merijn import merijn
+from dodo import function
+
+def teamName():
+    return "This is team READUS. We are: " + myName() + merijn() + function()
+
+print(teamName())
