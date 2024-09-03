7c11f1c01f2192abc5ccecb8919904c101fa7280
Jaccosf <69802108+Jaccosf@users.noreply.github.com>
2024-09-03 15:07:50 +0200

    format date

--- challenge_day2/station2.py
+++ challenge_day2/station2.py
@@ -1,18 +1,19 @@
 from datetime import datetime
 
 def solution_station_2(date):
-    date.weekday()
-    if date.weekday() == 0:
+    formatted_date = datetime.strptime(date, '%Y-%m-%d')
+    day = formatted_date.weekday()
+    if day == 0:
         return "月曜日"
-    elif date.weekday() == 1:
+    elif day == 1:
         return "火曜日"
-    elif date.weekday() == 2:
+    elif day == 2:
         return "水曜日"
-    elif date.weekday() == 3:
+    elif day == 3:
         return "木曜日"
-    elif date.weekday() == 4:
+    elif day == 4:
         return "金曜日"
-    elif date.weekday() == 5:
+    elif day == 5:
         return "土曜日"
-    elif date.weekday() == 6:
+    elif day == 6:
         return "日曜日"
