a88a7767ff13a037e1ba9140558292876ba24775
Jaccosf <69802108+Jaccosf@users.noreply.github.com>
2024-09-03 15:07:21 +0200

    fix fibonacci

--- challenge_day2/station4.py
+++ challenge_day2/station4.py
@@ -3,12 +3,8 @@ def solution_station_4(num):
     if num == 0 or num == 1:
         return False
     elif num > 1:
-    # check for factors
-        for i in range(2, num):
-            if (num % i) == 0:
-                # if factor is found, set flag to True
-                return True
-                # break out of loop
-                break
-            else:
-                return False
+        for i in range(2, (num//2)+1):
+                if (num % i) == 0:
+                    return False
+        else:
+            return True
