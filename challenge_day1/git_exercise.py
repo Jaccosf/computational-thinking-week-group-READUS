ed34be202b94cd8ed845a8606b24b09c983c80cf
beythas <bey.thas@gmail.com>
2024-09-05 10:07:56 +0200

    Update git_exercise.py

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,8 +1,9 @@
 from jacco import myName
 from merijn import merijn
 from dodo import function
+from bey import bey
 
 def teamName():
-    return "This is team READUS. We are: " + myName() + ", " + merijn() + ", " + function()
+    return "This is team READUS. We are: " + myName() + ", " + merijn() + ", " + function() + ", " + bey()
 
 print(teamName())
