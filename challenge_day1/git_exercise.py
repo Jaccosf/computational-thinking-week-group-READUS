0601b0e900f277bee840ca79da6b0ed052c94e3c
Jaccosf <69802108+Jaccosf@users.noreply.github.com>
2024-09-05 16:10:36 +0200

    update basefile story

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,9 +1,13 @@
-from jacco import myName
-from merijn import merijn
-from dodo import function
-from bey import bey
+from jacco import *
+from merijn import *
+from dodo import *
+from bey import *
 
 def teamName():
     return "This is team READUS. We are: " + myName() + ", " + merijn() + ", " + function() + ", " + bey()
 
 print(teamName())
+
+print(paragraph1() + "\n" + merijn_act_1() + "\n" + act1p3() + "\n" + dodo_act_1())
+print(paragraph2() + "\n" + merijn_act_2() + "\n" + act2p3() + "\n" + dodo_act_2())
+print(paragraph3() + "\n" + merijn_act_3() + "\n" + act3p3() + "\n" + dodo_act_3())
