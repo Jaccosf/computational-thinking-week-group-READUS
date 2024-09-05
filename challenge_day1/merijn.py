b297188cc0c6f299af7c0821bef11c0f689eeb42
merijnvervoorn <161606054+merijnvervoorn@users.noreply.github.com>
2024-09-05 16:09:30 +0200

    Adding my acts (with errors)

--- challenge_day1/merijn.py
+++ challenge_day1/merijn.py
@@ -1,2 +1,14 @@
 def merijn():
     return "Merijn"
+
+def char_3():
+    return "{char_3}"
+
+def merijn_act_1():
+    return f"{char_3} couldn’t step thinking about {char_2}’s death. He replayed every conversation he’d had with {char_2} and tried to figure out who might want him dead. {char_1} insisted {char_3} was hiding something, but {char_4} pointed out that {char_1} was quick to blame someone else. Meanwhile, {char_4} was keeping a low profile, acting as if he wanted nothing to do with the drama."
+
+def merijn_act_2():
+    return f"{char_3} shat back at {char_1}, asking why {char_1}’s brother was so eager to cover for him. {char_1}’s face got red, and he accused {char_3} of grasping at straws. {char_4} chimed in, saying that {char_3}’s alibi seemed too convenient, like he was trying to seem innocent. {char_3} was getting frustrated, feeling trapped between {char_1} and {char_4}’s suspicions."
+
+def merijn_act_3():
+    return f"{char_3} and {char_1} pressed {char_4} harder, sking why he lied about the gym. {char_4} finally broke down and admitted his alibi was fake, but he insisted he didn’t kill {char_2}. He said he was with someone he shouldn’t have been and didn’t want anyone to know. {char_1} and {char_3} exchanged a look — {char_4} was lying about one thing, but was he lying about the murder?"
