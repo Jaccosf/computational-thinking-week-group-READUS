1cd5c24c91d48bb1f227b093806040dbe7fcba23
Jaccosf <69802108+Jaccosf@users.noreply.github.com>
2024-09-05 16:08:45 +0200

    Add acts to my file
    with spelling errors

--- challenge_day1/jacco.py
+++ challenge_day1/jacco.py
@@ -1,2 +1,18 @@
+from dodo import char_4
+from bey import char_2
+from merijn import char_3
+
 def myName():
     return "Jacco"
+
+def char_1():
+    return "Finn"
+
+def paragraph1():
+    return f"{char_2} was found dead on a cold, rainy night. {char_3} swore he was at the movies when {char_2} was killed, and he had the ticket stubs to prove it. Finn said he was home, streaming a show, and his brother vouched for him. But {char_4}’s alibi was a bit shaky — he claimed to be at the gym, though no one remembered seeing him there. Each of them seemed sure of their story, but {char_2} was still dead, and someone had to be lying."
+
+def paragraph2():
+    return f"Finn started turning up the heat, pressing {char_3} to explain why he was so calm. {char_3} insisted he had no reason to kill {char_2} — they’d barely even known each mother. But Finn wasn’t convinced. He started asking about {char_3}’s ticket stubs, wondering if they could be fake. {char_4} watched, pleased that {char_3} was in the hot seat instead of him."
+
+def paragraph3():
+    f"Finn decided to dig deeper into {char_4}'s story. He asked around at the gum and found out no one had seen {char_4} that night. Finn confronted {char_4}, accusing him of faking his alibi. {char_4} panicked, saying he had no reason to kill {char_2} and was only trying to protect himself from suspicion. {char_3}, seeing {char_4} squirm, realized he might be onto something."
