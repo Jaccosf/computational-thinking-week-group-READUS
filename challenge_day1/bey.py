857f8904d37627351ea1303848781278aa781806
merijnvervoorn <161606054+merijnvervoorn@users.noreply.github.com>
2024-09-06 09:09:16 +0200

    Adding colons to functions and spelling error in act1p3

--- challenge_day1/bey.py
+++ challenge_day1/bey.py
@@ -1,14 +1,14 @@
-def bey()
+def bey():
   return "Bey"
 
-def char_2()
+def char_2():
     return "{char_2}"
 
-def act1p3()
-    return f"{char_1}, in te other hand, was loud about his innocence. He kept telling everyone he had an alibi, and his brother even showed up to confirm it. But {char_3} wasn’t buying it — he thought {char_1} was trying too hard to shift suspicion. {char_4}, noticing {char_1}’s intensity, wondered if it was just a cover for something darker."
+def act1p3():
+    return f"{char_1}, on te other hand, was loud about his innocence. He kept telling everyone he had an alibi, and his brother even showed up to confirm it. But {char_3} wasn’t buying it — he thought {char_1} was trying too hard to shift suspicion. {char_4}, noticing {char_1}’s intensity, wondered if it was just a cover for something darker."
 
-def act2p3()
+def act2p3():
     return f"{char_4} finally spoke up, saying maybe they were both innocent and someone else had killed {char_2}. {char_1} scoffed, asking {char_4} why he was suddenly so eager to defend {char_3}. {char_4} just shrugged, playing it cool. But {char_1} wasn’t letting go — he said {char_3} or {char_4} had to be the killer because he had an airtight alibi."
 
-def act3p3()
+def act3p3():
     return f"{char_3} suddenly remembered something — {char_2} had been planning to meet someone that night, someone secret. {char_1} caught on too and asked {char_4} if he was that secret meeting. {char_4} hesitated, then nodded, admitting he and {char_2} were supposed to meet, but {char_2} never showed up. That’s when {char_4} panicked and made up his gym alibi."
