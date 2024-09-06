e499e9e6fb87ba4d838456963857d24da50f4843
Dodcsi <dodo.baldy.com>
2024-09-06 12:55:55 +0200

    mistakes corrected

--- challenge_day1/merijn.py
+++ challenge_day1/merijn.py
@@ -9,7 +9,7 @@ def char_3():
     return "Marco"
 
 def merijn_act_1():
-    return f"{char_3} couldn’t step thinking about {char_2}’s death. He replayed every conversation he’d had with {char_2} and tried to figure out who might want him dead. {char_1} insisted {char_3} was hiding something, but {char_4} pointed out that {char_1} was quick to blame someone else. Meanwhile, {char_4} was keeping a low profile, acting as if he wanted nothing to do with the drama."
+    return f"{char_3} couldn’t stop thinking about {char_2}’s death. He replayed every conversation he’d had with {char_2} and tried to figure out who might want him dead. {char_1} insisted {char_3} was hiding something, but {char_4} pointed out that {char_1} was quick to blame someone else. Meanwhile, {char_4} was keeping a low profile, acting as if he wanted nothing to do with the drama."
 
 def merijn_act_2():
     return f"{char_3} shot back at {char_1}, asking why {char_1}’s brother was so eager to cover for him. {char_1}’s face got red, and he accused {char_3} of grasping at straws. {char_4} chimed in, saying that {char_3}’s alibi seemed too convenient, like he was trying to seem innocent. {char_3} was getting frustrated, feeling trapped between {char_1} and {char_4}’s suspicions."
