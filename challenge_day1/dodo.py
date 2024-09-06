1634cb94f543fbd567b24733d5bb549d4156b35b
Dodcsi <dodo.baldy.com>
2024-09-06 10:49:37 +0200

    hehe

--- challenge_day1/dodo.py
+++ challenge_day1/dodo.py
@@ -1,2 +1,16 @@
 def function():
     return "Dodo"
+
+def char_4():
+    return "Taylor"
+
+def paragraph_1():
+    return f"{char_4}, however, was quietly sweating. His gym alibi was fake, and he knew it wouldn’t hold up under scrutiny. Yet, he kept his cool, watching {char_3} and {char_1} tear each other apart. All {char_4} needed was for {char_3} and {char_1} to keep pointing fingers at each other, just long enough for the truth to get buried.
+"
+
+def paragraph_2():
+    return f"{char_3}, feeling the pleasure, started to crack. He admitted he had doubts about {char_4}’s gym story. {char_1} jumped on this, asking {char_4} if anyone could verify his whereabouts. {char_4}, realizing his fake alibi might be exposed, got defensive. He said it was just bad luck that no one saw him at the gym — but {char_3} and {char_1} weren’t buying it.
+"
+
+def paragraph_3():
+    return f"With {char_4}'s confetion, {char_3} and {char_1} realized the truth. {char_4} had lied, but he wasn’t the killer. {char_2} never met {char_4} because {char_2} had already been dead. {char_3}, relieved but wary, glanced at {char_1} — was it {char_1} all along, or was there someone else they hadn’t considered? {char_1} just smiled, his alibi still holding strong, as the mystery deepened."
